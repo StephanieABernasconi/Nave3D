9ef06928272710aaa186cd1becb89afd0cc5bec8
StephanieABernasconi <stephanieabernasconi@gmail.com>
2022-10-31 15:01:00 -0300

    nave 3D

---
+++ app.js
@@ -0,0 +1,66 @@
+//Variables for setup
+
+let container;
+let camera;
+let renderer;
+let scene;
+let nave;
+
+function init() {
+    console.log("teste")
+  container = document.querySelector(".scene");
+
+  //Create scene
+  scene = new THREE.Scene();
+
+  const fov = 35;
+  const aspect = container.clientWidth / container.clientHeight;
+  const near = 0.1;
+  const far = 1000;
+ console.log(container)
+  //Camera setup
+  camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+  camera.position.set(0, 0, 50);
+
+  const ambient = new THREE.AmbientLight(0x404040, 2);
+  scene.add(ambient);
+
+  const light = new THREE.DirectionalLight(0xffffff, 2);
+  light.position.set(10, 50, 100);
+  scene.add(light);
+
+  const light2 = new THREE.DirectionalLight(0xffffff, 5);
+  light2.position.set(100, 50, 100);
+  scene.add(light2);
+  //Renderer
+  renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+  renderer.setSize(container.clientWidth, container.clientHeight);
+  renderer.setPixelRatio(window.devicePixelRatio);
+
+  container.appendChild(renderer.domElement);
+
+  //Load Model
+  let loader = new THREE.GLTFLoader();
+  loader.load("./nave/scene.gltf", function(gltf) {
+    scene.add(gltf.scene);
+    house = gltf.scene.children[0];
+    animate();
+  });
+}
+
+function animate() {
+  requestAnimationFrame(animate);
+  house.rotation.z += 0.001;
+  renderer.render(scene, camera);
+}
+
+init();
+
+function onWindowResize() {
+  camera.aspect = container.clientWidth / container.clientHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(container.clientWidth, container.clientHeight);
+}
+
+window.addEventListener("resize", onWindowResize);
